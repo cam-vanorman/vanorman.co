9c979b4e6492709dcb5de8390dc3737ee973dec2
Cameron Van Orman <cameron@efellemedia.com>
2019-06-10 20:38:31 -0700

    Updating master template

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -19,7 +19,23 @@
         @stack('meta')
 
         @if ($page->production)
-            <!-- Insert analytics code here -->
+
+            @if (getenv('GA_ID'))
+                {{-- Global site tag (gtag.js) - Google Analytics --}}
+                <script async src="https://www.googletagmanager.com/gtag/js?id={{ getenv('GA_ID') }}"></script>
+                <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', {{ getenv('GA_ID') }});
+                </script>
+            @endif
+
+            @if (getenv('RECAPTCHA_SITE_KEY'))
+                {{-- Recaptcha --}}
+                <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+            @endif
         @endif
 
         <link href="https://fonts.googleapis.com/css?family=Cabin:300,400,800" rel="stylesheet">
