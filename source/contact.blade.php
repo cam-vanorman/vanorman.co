7e75dc0a4f58bd273f53818557b6354265da46e3
Cameron Van Orman <cameron@efellemedia.com>
2019-06-10 20:38:15 -0700

    Updating contact with captcha and form classes

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -1,5 +1,7 @@
 @extends('_layouts.master')
 
+@php($contact = getenv('CONTACT_FORM') ?? getenv('CONTACT_FORM'))
+
 @push('meta')
     <meta property="og:title" content="Contact {{ $page->siteName }}" />
     <meta property="og:type" content="website" />
@@ -10,14 +12,10 @@
 @section('body')
 <h1>Contact</h1>
 
-<p class="mb-8">
-    
-</p>
-
-<form action="/contact" class="mb-12">
+<form action="{!! $contact !!}" method="POST" class="mb-12">
     <div class="flex flex-wrap mb-6 -mx-3">
         <div class="w-full md:w-1/2 mb-6 md:mb-0 px-3">
-            <label class="block mb-2 text-grey-darkest text-sm font-semibold" for="contact-name">
+            <label class="form__label" for="contact-name">
                 Name
             </label>
 
@@ -26,13 +24,13 @@
                 id="contact-name"
                 placeholder="Jane Doe"
                 name="name"
-                class="block w-full border shadow rounded-lg outline-none mb-2 p-4"
+                class="form__input"
                 required
             >
         </div>
 
         <div class="w-full px-3 md:w-1/2">
-            <label class="block text-grey-darkest text-sm font-semibold mb-2" for="contact-email">
+            <label class="form__label" for="contact-email">
                 Email Address
             </label>
 
@@ -41,14 +39,14 @@
                 id="contact-email"
                 placeholder="email@domain.com"
                 name="email"
-                class="block w-full border shadow rounded-lg outline-none mb-2 p-4"
+                class="form__input"
                 required
             >
         </div>
     </div>
 
     <div class="w-full mb-12">
-        <label class="block text-grey-darkest text-sm font-semibold mb-2" for="contact-message">
+        <label class="form__label" for="contact-message">
             Message
         </label>
 
@@ -56,17 +54,22 @@
             id="contact-message"
             rows="4"
             name="message"
-            class="block w-full border shadow rounded-lg outline-none appearance-none mb-2 px-4 py-6"
+            class="form__text"
             placeholder="A lovely message here."
             required
         ></textarea>
+    
+        @if(getenv('RECAPTCHA_SITE_KEY'))
+            <div class="mt-6 g-recaptcha" id="g-recaptcha" data-sitekey="{{ getenv('RECAPTCHA_SITE_KEY') }}"></div>
+        @endif
     </div>
 
     <div class="flex justify-end w-full">
+
         <input
             type="submit"
             value="Submit"
-            class="block bg-blue hover:bg-blue-dark text-white text-sm font-semibold tracking-wide uppercase shadow rounded-lg cursor-pointer px-6 py-3"
+            class="btn"
         >
     </div>
 </form>
