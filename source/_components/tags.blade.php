c3d2364fb685d8fbd6ca4d8507eaa13ba4cadac5
Cameron Van Orman <cameron@efellemedia.com>
2019-09-22 16:02:56 -0700

    Updating tag spacing and removing uppercase letters

--- source/_components/tags.blade.php
+++ source/_components/tags.blade.php
@@ -2,6 +2,6 @@
 <div class="w-full block mb-5 mx-auto text-center">
   <h3>Built With</h3>
   @foreach($tags as $tag)
-    <span class="inline-flex rounded-full bg-tertiary text-steel-blue uppercase px-2 py-1 text-xs mr-3">{{ $tag }}</span>
+    <span class="inline-flex rounded-full bg-tertiary text-steel-blue px-2 py-1 text-xs mr-1">{{ $tag }}</span>
   @endforeach
 </div>
